ccdba2a95dce818ab5547d5cb89d53af8a0327f3
Gustavo Miller Santos <gustavo.miller@quintoandar.com.br>
2023-04-16 01:28:06 -0300

    feat: create setup.py

---
+++ setup.py
@@ -0,0 +1,8 @@
+from distutils.core import setup
+
+setup(name='the-week-in-chess-python-client',
+      version='0.1.0',
+      description='Python client for The Week in Chess',
+      author='Gustavo Miller Santos',
+      packages=['src'],
+)
