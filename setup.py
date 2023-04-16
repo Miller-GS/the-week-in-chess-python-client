a9fee183364d79e4689851d6c520e7e4a212e61c
Gustavo Miller Santos <gustavo.miller@quintoandar.com.br>
2023-04-16 01:28:07 -0300

    lint: setup

--- setup.py
+++ setup.py
@@ -1,8 +1,9 @@
 from distutils.core import setup
 
-setup(name='the-week-in-chess-python-client',
-      version='0.1.0',
-      description='Python client for The Week in Chess',
-      author='Gustavo Miller Santos',
-      packages=['twic'],
+setup(
+    name="the-week-in-chess-python-client",
+    version="0.1.0",
+    description="Python client for The Week in Chess",
+    author="Gustavo Miller Santos",
+    packages=["twic"],
 )
