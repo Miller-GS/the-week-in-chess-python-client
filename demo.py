e7a1e881558c3baa2c29e09bf0edf2ea0ed39970
Gustavo Miller Santos <gustavo.miller@quintoandar.com.br>
2023-05-07 18:05:18 -0300

    fix: move demo to correct folder

--- demo.py
+++ demo.py
@@ -9,8 +9,8 @@ for date, url in list(available_pgn_game_urls.items())[:10]:
 
 desired_date = input("Enter the date of the game you want to download (YYYY-MM-DD): ")
 
-try:
-    pgn_game = client.download_pgn_from_date(desired_date)
-    print(pgn_game)
-except ValueError:
-    print("No PGN game available for the given date.")
+# try:
+pgn_game = client.download_pgn_from_date(desired_date)
+print(pgn_game)
+# except ValueError:
+    # print("No PGN game available for the given date.")
